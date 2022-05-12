6ae40b1eb0cfb8062b05329940a78a0aab0b77db
IFofone <tomasfojt@seznam.cz>
2022-05-12 22:53:17 +0200

    Added FontAwesome icons

--- Lumeer/AssemblyInfo.cs
+++ Lumeer/AssemblyInfo.cs
@@ -1,3 +1,7 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("FontAwesomeRegular.otf", Alias = "FAR")]
+[assembly: ExportFont("FontAwesomeSolid.otf", Alias = "FAS")]
